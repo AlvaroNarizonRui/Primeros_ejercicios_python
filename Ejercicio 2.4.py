52327a08a83524cd33996d63b66d34b6c99078f9
Alvaro Rui <aruigal2303@g.educaand.es>
2023-10-04 13:08:26 +0200

    ejercicios organizados sin tests todavia

--- Ejercicio 2.4.py
+++
@@ -1,7 +0,0 @@
-"""
-Ejercicio 2.4¶
-Escribe un programa que le pida al usuario una temperatura en grados Celsius, la convierta a grados Fahrenheit e imprima por pantalla la temperatura convertida.
-"""
-temperatura_celsius = float(input("Escribe una temperatura en grados celsius : "))
-fahrenheit = (temperatura_celsius*9/5)+32
-print(f"{temperatura_celsius}º son {fahrenheit}Fº")
