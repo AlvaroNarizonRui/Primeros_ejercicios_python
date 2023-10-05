553688d590bfea66ff2b6ded8d20c754a89b5fca
Alvaro <aruigal2303@g.educaand.es>
2023-10-05 12:21:52 +0200

    Refactorizados todos los archivos para mayor flexibilidad

--- src/ejercicio2.1.py
+++
@@ -1,6 +0,0 @@
-"""
-Ejercicio 2.1
-Escribe un programa que pida el nombre del usuario para luego darle la bienvenida.
-"""
-nombre = input("Escribe tu nombre : ")
-print(f"Hola, {nombre}")
--- src/ejercicio2.12.py
+++
@@ -1,8 +0,0 @@
-"""Ejercicio 2.12¶
-Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, 
-y muestre por pantalla la frase Tu índice de masa corporal es donde es el índice de masa corporal calculado redondeado con dos decimales."""
-
-peso = float(input("Escribe tu peso en kg : "))
-estatura = float(input("Escribe tu estatura en metros : "))
-imc = peso/estatura**2
-print("Tu IMC es : ", round(imc,2))
