23efff964164234f64851429e55f05cc041ed34a
Alvaro <aruigal2303@g.educaand.es>
2023-10-05 10:28:59 +0200

    Subido ejercicio 2.2 arreglado

--- src/ejercicio2.2.py
+++ src/ejercicio2.2.py
@@ -2,7 +2,14 @@
 Ejercicio 2.2
 Escribe un programa para pedirle al usuario las horas de trabajo y el precio por hora y calcule el importe total del servicio.
 """
+def calcularImporte(horas,precio) -> float:
+    importe = horas * precio
+    return importe
+
+#Entrada
 horas = int(input("Escribe las horas de trabajo : "))
 precio = int(input("Escribe el precio por hora de trabajo : "))
-importe = horas * precio
+#Procesamiento
+importe = calcularImporte(horas,precio)
+#Salida
 print(f"Tu importe total por el servicio es de {importe} euros")
